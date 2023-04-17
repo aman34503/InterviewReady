6c91f13673f3ba38c0c03751c20db38c9472acbd
aman shrivastava <aman34503@gmail.com>
2023-04-18 00:51:43 +0530

    change update file

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,3 +1,20 @@
+// import firebase from "firebase/compat/app"
+// import "firebase/compat/auth"
+
+// export default function useFirebase() {
+//   const config = {
+//     apiKey: "AIzaSyDJnlC0oO_fgAEleI-isdHn29MXDyaxL1Y",
+//     authDomain: "interviewready-95f7b.firebaseapp.com",
+//     projectId: "interviewready-95f7b",
+//     storageBucket: "interviewready-95f7b.appspot.com",
+//     messagingSenderId: "590253887992",
+//     appId: "1:590253887992:web:d756df491555222910860d",
+//   }
+//   firebase.initializeApp(config)
+//   return firebase
+// }
+
+
 import firebase from "firebase/compat/app"
 import "firebase/compat/auth"
 
